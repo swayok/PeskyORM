598d7f3161ca48c1a1b400b2cb3e4a05b43231dc
Filippov Aleksandr <swayalex@gmail.com>
2016-10-07 20:12:10 +0300

    Updated sql files

--- tests/configs/pgsql_new_db_and_role.sql
+++ tests/configs/pgsql_new_db_and_role.sql
@@ -1,8 +1,6 @@
-CREATE DATABASE "pesky_orm_test_db" UTF8
-
 CREATE ROLE "pesky_orm_test" login PASSWORD '1111111' NOINHERIT;
 
-GRANT ALL ON DATABASE "pesky_orm_test_db" TO "pesky_orm_test";
+CREATE DATABASE "pesky_orm_test_db" ENCODING 'UTF8' OWNER "pesky_orm_test";
 
 -- make sure it is not free for all access in pg_hba.conf
 
