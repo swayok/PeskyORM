5f0b60b10459a81a9d3bc2a7d4f4f25b6164d865
Alexander Filippov <swayalex@gmail.com>
2019-01-23 16:16:10 +0300

    Removed min/max length restrictions from IP field

--- src/PeskyORM/DbObjectField/IpField.php
+++ src/PeskyORM/DbObjectField/IpField.php
@@ -10,13 +10,13 @@ class IpField extends StringField {
     const IPV4_MIN_LENGTH = 7;
     const IPV4_MAX_LENGTH = 15;
 
-    public function getMinLength() {
-        return self::IPV4_MIN_LENGTH;
-    }
-
-    public function getMaxLength() {
-        return self::IPV4_MAX_LENGTH;
-    }
+//    public function getMinLength() {
+//        return self::IPV4_MIN_LENGTH;
+//    }
+//
+//    public function getMaxLength() {
+//        return self::IPV4_MAX_LENGTH;
+//    }
 
     public function isValidValueFormat($value, $silent = true) {
         if (empty($value) || ValidateValue::isIpAddress($value)) {
