041051c6dd5132329121a58552eccc3218b638d6
Alexander Filippov <swayalex@gmail.com>
2022-08-11 13:04:27 +0300

    ValueToObjectConverterInterface - removed createObjectFromObject()

--- src/ORM/ValueToObjectConverter.php
+++ src/ORM/ValueToObjectConverter.php
@@ -16,7 +16,4 @@ abstract class ValueToObjectConverter implements ValueToObjectConverterInterface
         $this->other = $unknownProperties;
     }
     
-    public static function createObjectFromObject($object) {
-        throw new \BadMethodCallException('Cannot convert object of class ' . get_class($object). ' to object of class ' . static::class);
-    }
 }
--- src/ORM/ValueToObjectConverterInterface.php
+++ src/ORM/ValueToObjectConverterInterface.php
@@ -4,15 +4,10 @@ namespace PeskyORM\ORM;
 
 interface ValueToObjectConverterInterface
 {
+    
     /**
      * @return static
      */
     public static function createObjectFromArray(array $data);
     
-    /**
-     * Convert other $object to object of this class
-     * @param object $object
-     * @return static
-     */
-    public static function createObjectFromObject($object);
 }
